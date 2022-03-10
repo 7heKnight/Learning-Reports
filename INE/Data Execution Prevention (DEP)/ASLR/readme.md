db8eea3682b91e3452dfa6ec04d0ce681accfaba
Ly Tuan Kiet <60308445+7heKnight@users.noreply.github.com>
2022-03-10 22:50:38 +0700

    Delete INE/Data Execution Prevention (DEP) directory

--- INE/Data Execution Prevention (DEP)/ASLR/readme.md
+++
@@ -1,2 +0,0 @@
-# Lazy writing report, so skip it :P
-## Depending on NX bit bypass technique, and using brute force to bypass it!
--- INE/Data Execution Prevention (DEP)/ASLR/run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash -p
-while true; 
-do 
-
-  (./exploit.py ; echo id; echo ls) | ./vulnerable | grep 'uid' -A 10 | tr 'A' ' ' ; 
-
-done
